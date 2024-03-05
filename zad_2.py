17d81012679adcaaa7b23789cdb00e32342a55d0
Adam <adamos139@wp.pl>
2024-03-05 16:57:53 +0100

    zadanie 2 PEP8

--- zad_2.py
+++ zad_2.py
@@ -1,28 +1,32 @@
 def fun_a(A: list):
-	for i in A:
-		print(i)
+    for i in A:
+        print(i)
+
 
 def fun_b1(B: list) -> list:
-	BB = []
-	for i in B:
-		BB.append(i * 2)
-	return BB
+    BB = []
+    for i in B:
+        BB.append(i * 2)
+    return BB
+
 
 def fun_b2(B: list) -> list:
-	return [k * 2 for k in B]
+    return [k * 2 for k in B]
+
 
 def fun_c(C: list):
-	for j in C:
-		if j % 2 == 0:
-			print(j)
+    for j in C:
+        if j % 2 == 0:
+            print(j)
+
 
 def fun_d(D):
-	for l in D[1::2]:
-		print(l)
+    for la in D[1::2]:
+        print(la)
 
 
-fun_a("Adam","Adam","Adam","Aneta","Al")
-print(fun_b1([0,1,2,3,4]))
-print(fun_b2([1,2,3,4,5]))
+fun_a("Adam", "Adam", "Adam", "Aneta", "Al")
+print(fun_b1([0, 1, 2, 3, 4]))
+print(fun_b2([1, 2, 3, 4, 5]))
 fun_c(range(10))
 fun_d(range(10))
